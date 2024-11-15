fb108573cdd917cc5b6640739ee34d57572f9c9b
taidalog <72747279+taidalog@users.noreply.github.com>
2024-11-15 22:40:58 +0900

    Added `dotnet new gitignore` to `init.pfs1`. #4

--- init.ps1
+++ init.ps1
@@ -1,6 +1,7 @@
 dotnet new console --language "F#" --name "App" --framework "net8.0" --output src
 dotnet new globaljson --sdk-version "8.0.100" --roll-forward latestFeature
 dotnet new nugetconfig
+dotnet new gitignore
 dotnet new tool-manifest
 dotnet tool install fable
 dotnet tool install fantomas
