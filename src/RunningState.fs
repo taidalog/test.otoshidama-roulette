07aa86fa2f8bde68e3697b0a0a81c0f0e4fd08ed
taidalog <72747279+taidalog@users.noreply.github.com>
2024-11-17 16:18:38 +0900

    Added `RunningState` module to represent the running state of the page. #7

---
+++ src/RunningState.fs
@@ -0,0 +1,11 @@
+// otoshidama-radix Version 0.1.0
+// https://github.com/taidalog/otoshidama-radix
+// Copyright (c) 2023-2024 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/otoshidama-radix/blob/main/LICENSE
+namespace OtoshidamaRadix
+
+module RunningState =
+    type RunningState =
+        | Stopping
+        | Running of int * string * string
