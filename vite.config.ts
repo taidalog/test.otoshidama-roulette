6d066e1af29655c7a8372e46fc179e4663001d8b
taidalog <72747279+taidalog@users.noreply.github.com>
2024-11-24 11:14:34 +0900

    Renamed the project "otoshidama-roulette". Close #13.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/otoshidama-redix/',
+    base: '/otoshidama-roulette/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
