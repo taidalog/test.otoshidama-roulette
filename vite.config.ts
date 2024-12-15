d68adb482c82666e00db505eaff647b64b3d96c7
taidalog <72747279+taidalog@users.noreply.github.com>
2024-12-16 00:11:14 +0900

    Fixed the base path for the test page. #17

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/otoshidama-roulette/',
+    base: '/test.otoshidama-roulette/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
