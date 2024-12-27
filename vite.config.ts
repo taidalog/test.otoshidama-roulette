4db28ca2aefbd4e055a8b1626b20bd1056f768ec
taidalog <72747279+taidalog@users.noreply.github.com>
2024-12-27 22:07:21 +0900

    Changed `vite.config.ts` so that it works with `.env`. #33
    Finally it works!

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: import.meta.env.VITE_BASE_URL,
-    build: {
-        outDir: 'docs',
-        emptyOutDir: true
-    },
-    envDir: 'env',
-    clearScreen: false,
-    server: {
-        port: 8080,
-        watch: {
-            ignored: [
-                "**/*.fs" // Don't watch F# files
-            ]
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        base: env.VITE_BASE_URL,
+        build: {
+            outDir: 'docs',
+            emptyOutDir: true
+        },
+        envDir: 'env',
+        clearScreen: false,
+        server: {
+            port: 8080,
+            watch: {
+                ignored: [
+                    "**/*.fs" // Don't watch F# files
+                ]
+            }
         }
     }
 })
